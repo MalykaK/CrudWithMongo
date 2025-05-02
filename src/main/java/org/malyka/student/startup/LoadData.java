bc8f22c840614a2bf39726b2c5b6d4efd4fe5c4d
malyka78@gmail.com <sdsts28@outlook.com>
2025-05-02 14:41:42 -0400

    Load  data from json in resources. Parsed data with Object Mapper and pushed to DB.

---
+++ src/main/java/org/malyka/student/startup/LoadData.java
@@ -0,0 +1,37 @@
+package org.malyka.student.startup;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import jakarta.annotation.PostConstruct;
+import org.malyka.student.model.Student;
+import org.malyka.student.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+//Understand this below annotation
+@Configuration
+public class LoadData {
+    ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new ParameterNamesModule())
+            .registerModule(new Jdk8Module())
+            .registerModule(new JavaTimeModule());
+
+    //ask chatgpt to explain like a kid
+    @Autowired
+    StudentRepo studentRepo;
+
+    //learn this
+    @PostConstruct
+    public void readMyStudentData() throws IOException {
+        // Practice with more models
+        var studentList = objectMapper.readValue(new File("src/main/resources/jsonData.json"), Student[].class);
+        studentRepo.deleteAll();
+        studentRepo.saveAll(Arrays.asList(studentList));
+    }
+}
